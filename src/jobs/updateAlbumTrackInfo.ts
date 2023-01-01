a5b45ed1df4274855069da5910cedc906ca3d76b
Mike Farrow <mike@farrow.io>
2023-01-01 16:30:19 +0000

    fixing typo

--- src/jobs/updateAlbumTrackInfo.ts
+++ src/jobs/updateAlbumTrackInfo.ts
@@ -1,16 +1,13 @@
-
 import log from '@/logger';
 import { AlbumRepository } from '@/db';
 
 export default async function () {
-    log('udpating album track meta' );
+  log('udpating album track meta');
 
-    const albums = await AlbumRepository.getAll()
-      .toPromise();
+  const albums = await AlbumRepository.getAll().toPromise();
 
-    for(const album of albums) {
-      await album.updateTrackInfo();
-      await album.save();
-    }
-  };
+  for (const album of albums) {
+    await album.updateTrackInfo();
+    await album.save();
+  }
 }
